044d91920dc84bb2ec2b19cd69bf70c661fec664
Viacheslav Shalygin <96007284+vshalygin@users.noreply.github.com>
2025-10-26 14:21:41 +0300

    Add guarded_value class to common_lib

---
+++ common-lib-test/src/utils/guarded-value/guarded-value-test.cpp
@@ -0,0 +1,42 @@
+#include <common-lib/utils/guarded-value/guarded-value.h>
+
+#include <gtest/gtest.h>
+
+using namespace vsh::common_lib;
+using namespace testing;
+
+namespace {
+    class test_class
+    {
+    public:
+        explicit test_class(int numb)
+            : number(numb)
+        {}
+
+        test_class(test_class &) = delete;
+        test_class &operator=(test_class &) = delete;
+
+        test_class(test_class &&) = default;
+        test_class &operator=(test_class &&) = default;
+
+        int number;
+    };
+}
+
+TEST(GuardedValue, MayBeCreatedByMovingValue)
+{
+    test_class obj(34);
+
+    guarded_value sut(std::move(obj));
+    auto [guard, val] = sut.get();
+
+    ASSERT_EQ(val.number, 34);
+}
+
+TEST(GuardedValue, MayBeCreatedByValueCreationArguments)
+{
+    guarded_value<test_class> sut(34);
+    auto [guard, val] = sut.get();
+
+    ASSERT_EQ(val.number, 34);
+}
---
+++ common-lib/common-lib/utils/guarded-value/guarded-value.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <mutex>
+#include <utility>
+
+namespace vsh::common_lib {
+    template<typename T>
+    class guarded_value final
+    {
+    public:
+        guarded_value(T &&value)
+            : value_(std::move(value))
+        {}
+
+        template<typename...Args>
+        guarded_value(Args&&...args)
+            : value_(std::forward<Args>(args)...)
+        {}
+
+        guarded_value(guarded_value &) = delete;
+        guarded_value &operator=(guarded_value &) = delete;
+
+        std::pair<std::unique_lock<std::mutex>, T &> get()
+        {
+            return { std::unique_lock(mtx_), value_ };
+        }
+
+        std::pair<std::unique_lock<std::mutex>, const T &> get() const
+        {
+            return { std::unique_lock(mtx_), value_ };
+        }
+
+    private:
+        mutable std::mutex mtx_;
+        T value_;
+    };
+}
