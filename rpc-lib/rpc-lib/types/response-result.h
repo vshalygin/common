2cbebf15858d8d6563eaa7a9e7523c2b190de0a3
Viacheslav Shalygin <96007284+vshalygin@users.noreply.github.com>
2025-12-03 10:59:39 +0300

    Update response result

--- rpc-lib/rpc-lib/types/response-result.h
+++ rpc-lib/rpc-lib/types/response-result.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <string>
+#include <cassert>
+
 namespace vsh::rpc {
     enum class response_result : unsigned char
     {
         ok = 0,
+        canceled,
+        insufficient_rights,
+        request_parse_error,
         unknown_error
     };
 
@@ -14,5 +20,43 @@ namespace vsh::rpc {
     inline bool is_fail(response_result rc)
     {
         return !is_success(rc);
+    }
+
+    inline std::string to_string(response_result rc)
+    {
+        switch(rc) {
+            case response_result::ok:
+                return "ok";
+            case response_result::canceled:
+                return "canceled";
+            case response_result::insufficient_rights:
+                return "insufficient_rights";
+            case response_result::request_parse_error:
+                return "request_parse_error";
+            case response_result::unknown_error:
+                return "unknown_error";
+            default:
+                assert(!"unexpected response result");
+                return "unknown_error";
+        }
+    }
+
+    inline response_result response_result_from_string(const std::string &rc)
+    {
+        auto ans = response_result::unknown_error;
+        if(rc == "ok") {
+            ans = response_result::ok;
+        } else if(rc == "canceled") {
+            ans = response_result::canceled;
+        } else if(rc == "insufficient_rights") {
+            ans = response_result::insufficient_rights;
+        } else if(rc == "request_parse_error") {
+            ans = response_result::request_parse_error;
+        } else if(rc == "unknown_error") {
+            ans = response_result::unknown_error;
+        } else {
+            assert(!"unexpected response result string");
+        }
+        return ans;
     }
 }
--- test/rpc-lib-test/src/types/response-result-test.cpp
+++ test/rpc-lib-test/src/types/response-result-test.cpp
@@ -8,11 +8,31 @@ using namespace testing;
 TEST(ResponseResult, TestIsSucceess)
 {
     EXPECT_TRUE(is_success(response_result::ok));
+    EXPECT_FALSE(is_success(response_result::canceled));
+    EXPECT_FALSE(is_success(response_result::insufficient_rights));
+    EXPECT_FALSE(is_success(response_result::request_parse_error));
     EXPECT_FALSE(is_success(response_result::unknown_error));
 }
 
 TEST(ResponseResult, TestIsFail)
 {
     EXPECT_FALSE(is_fail(response_result::ok));
+    EXPECT_TRUE(is_fail(response_result::canceled));
+    EXPECT_TRUE(is_fail(response_result::insufficient_rights));
+    EXPECT_TRUE(is_fail(response_result::request_parse_error));
     EXPECT_TRUE(is_fail(response_result::unknown_error));
+}
+
+TEST(ResponseResult, TestStringConversation)
+{
+    EXPECT_EQ(response_result::ok,
+              response_result_from_string(to_string(response_result::ok)));
+    EXPECT_EQ(response_result::canceled,
+              response_result_from_string(to_string(response_result::canceled)));
+    EXPECT_EQ(response_result::insufficient_rights,
+              response_result_from_string(to_string(response_result::insufficient_rights)));
+    EXPECT_EQ(response_result::request_parse_error,
+              response_result_from_string(to_string(response_result::request_parse_error)));
+    EXPECT_EQ(response_result::unknown_error,
+              response_result_from_string(to_string(response_result::unknown_error)));
 }
