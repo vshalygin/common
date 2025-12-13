e4656146b2607df0396164a01726276ea934de90
Viacheslav Shalygin <96007284+vshalygin@users.noreply.github.com>
2025-12-13 11:21:04 +0300

    Add spinlock

---
+++ common-lib/common-lib/syncronization/spinlock/spinlock.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <atomic>
+
+namespace vsh::cl {
+    class spinlock final
+    {
+    public:
+        spinlock() noexcept = default;
+
+        spinlock(spinlock &) = delete;
+        spinlock &operator=(spinlock &) = delete;
+
+        void lock() noexcept;
+        void unlock() noexcept;
+
+    private:
+        std::atomic_flag m_flag = ATOMIC_FLAG_INIT;
+    };
+}
