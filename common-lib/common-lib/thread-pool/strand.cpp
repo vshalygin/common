22381e7613a3d766875498d3487252256419a59b
Viacheslav Shalygin <96007284+vshalygin@users.noreply.github.com>
2026-06-08 13:56:50 +0300

    Add strand::dispatch

--- common-lib/common-lib/thread-pool/strand.cpp
+++ common-lib/common-lib/thread-pool/strand.cpp
@@ -3,23 +3,5 @@
 namespace vshalygin::cl {
     strand::strand(boost::asio::io_context &io_context)
         : m_strand(io_context.get_executor())
-        , m_executing_thread_id(std::make_shared<thread_id_t>())
     {}
-
-    bool strand::is_in_executing_context() const
-    {
-        auto executing_thread_id = m_executing_thread_id->load(std::memory_order_acquire);
-        return executing_thread_id == std::this_thread::get_id();
-    }
-
-    strand::thread_id_guard::thread_id_guard(std::shared_ptr<thread_id_t> thread_id)
-        : m_thread_id(std::move(thread_id))
-    {
-        m_thread_id->store(std::this_thread::get_id(), std::memory_order_release);
-    }
-
-    strand::thread_id_guard::~thread_id_guard()
-    {
-        m_thread_id->store(std::thread::id(), std::memory_order_release);
-    }
 }
--- test/common-lib-test/src/thread-pool/strand-test.cpp
+++ test/common-lib-test/src/thread-pool/strand-test.cpp
@@ -50,54 +50,47 @@ TEST(Strand, ExecuteTaskAfterDestruction)
     sync_event2.wait_for(std::chrono::seconds(10));
 }
 
-TEST(Strand, AnswerFalseOnCheckExecutingContextIfItIsNoIn)
-{
-    thread_pool pool(2);
-    auto sut = pool.create_strand();
 
-    ASSERT_FALSE(sut.is_in_executing_context());
-}
-
-TEST(Strand, AnswerTrueOnCheckExecutingContextIfItIsIn)
+TEST(Strand, DispatchesTaskByPostingInExecutionQueue)
 {
     event sync_event;
     thread_pool pool(2);
     auto sut = pool.create_strand();
+    InSequence s;
+    MockFunction<void()> mock1, mock2;
+    EXPECT_CALL(mock1, Call())
+        .Times(1);
+    EXPECT_CALL(mock2, Call())
+        .Times(1)
+        .WillOnce([&]() { sync_event.set(); });
 
-    sut.post([&]() {
-        ASSERT_TRUE(sut.is_in_executing_context());
-        sync_event.set();
-    });
-
+    sut.dispatch(mock1.AsStdFunction());
+    sut.dispatch(mock2.AsStdFunction());
     ASSERT_TRUE(sync_event.wait_for(std::chrono::seconds(10)));
 }
 
-TEST(Strand, AnswerTrueOnCheckExecutingInFunctorDestructor)
+TEST(Strand, DispatchesTaskByExecutingInPlace)
 {
-    class desctructor_checker
-    {
-    public:
-        desctructor_checker(event &sync_event, strand *strand)
-            : m_sync_event(sync_event)
-            , m_strand(strand)
-        {}
-
-        ~desctructor_checker()
-        {
-            EXPECT_TRUE(m_strand->is_in_executing_context());
-            m_sync_event.set();
-        }
-
-    private:
-        event &m_sync_event;
-        strand *m_strand;
-    };
-
-    event sync_event;
+    event sync_event1, sync_event2;
     thread_pool pool(2);
     auto sut = pool.create_strand();
+    InSequence s;
+    MockFunction<void()> mock1, mock2, mock3;
+    EXPECT_CALL(mock1, Call())
+        .Times(1)
+        .WillOnce([&]() {
+            sut.dispatch(mock2.AsStdFunction());
+        });
+    EXPECT_CALL(mock2, Call())
+        .Times(1)
+        .WillOnce([&]() { sync_event2.set(); });
+    EXPECT_CALL(mock3, Call())
+        .Times(1)
+        .WillOnce([&]() { sync_event1.set(); });
 
-    sut.post([checker = std::make_shared<desctructor_checker>(sync_event, &sut)]() {});
+    sut.dispatch(mock1.AsStdFunction());
+    ASSERT_TRUE(sync_event2.wait_for(std::chrono::seconds(10)));
+    sut.dispatch(mock3.AsStdFunction());
 
-    ASSERT_TRUE(sync_event.wait_for(std::chrono::seconds(10)));
+    ASSERT_TRUE(sync_event1.wait_for(std::chrono::seconds(10)));
 }
