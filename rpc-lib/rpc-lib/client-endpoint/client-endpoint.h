bd1f4bffbc5124778af293d9b4da83da47b136b8
Viacheslav Shalygin <96007284+vshalygin@users.noreply.github.com>
2026-06-19 15:43:53 +0300

    Remake client_endpoint

--- rpc-lib/rpc-lib/client-endpoint/client-endpoint.h
+++ rpc-lib/rpc-lib/client-endpoint/client-endpoint.h
@@ -1,9 +1,9 @@
 #pragma once
-#include "rpc-lib/channel/ichannel.h"
-#include "rpc-lib/service/iservice.h"
-#include "rpc-lib/connector/iconnector.h"
+#include "rpc-lib/channel/channel.h"
+#include "rpc-lib/service/service.h"
+#include "rpc-lib/connection/connection.h"
+#include "rpc-lib/connector/client-connector.h"
 #include "rpc-lib/channel/request-callback/request-callback.h"
-#include "rpc-lib/types/constants.h"
 #include "rpc-lib/types/request-exception.h"
 #include "rpc-lib/types/connection-state.h"
 
@@ -16,6 +16,10 @@
 #include <chrono>
 
 namespace vshalygin::rpc {
+    class iauthenticator;
+    class ipipe_env;
+
+    template<typename GServiceStub>
     class client_endpoint final
     {
         using state_change_callback_t = std::function<void(connection_state)>;
@@ -24,17 +28,18 @@ namespace vshalygin::rpc {
         using request_callback_t = std::function<void(request_result, std::unique_ptr<Response>)>;
 
         template<typename Response>
-        struct req_result_data
+        struct response_data
         {
-            request_result req_result = request_result::unknown_error;
+            request_result result = request_result::unknown_error;
             std::unique_ptr<Response> response;
         };
 
     public:
-        client_endpoint(std::shared_ptr<iservice> service,
-                        std::unique_ptr<ichannel> channel,
-                        std::unique_ptr<iconnector> connector,
+        template<typename GService>
+        client_endpoint(std::unique_ptr<GService> gservice,
                         std::shared_ptr<cl::thread_pool> thread_pool,
+                        std::shared_ptr<iauthenticator> authenticator,
+                        std::shared_ptr<ipipe_env> pipe_env,
                         state_change_callback_t &&handler,
                         const std::chrono::milliseconds &request_timeout);
 
@@ -47,12 +52,10 @@ namespace vshalygin::rpc {
 
         template<typename Request, typename Response>
         std::unique_ptr<Response> make_request(const Request &req,
-                                               auto &service_stub,
                                                auto method);
 
         template<typename Request, typename Response>
         void make_request_async(const Request &req,
-                                auto &service_stub,
                                 auto method,
                                 request_callback_t<Response> &&req_callback);
 
@@ -63,57 +66,101 @@ namespace vshalygin::rpc {
         std::shared_ptr<cl::thread_pool> m_thread_pool;
         std::shared_ptr<state_change_callback_t> m_change_callback;
         const std::chrono::milliseconds m_request_timeout;
+
+        GServiceStub m_service_stub;
     };
 
+    template<typename GServiceStub>
+    template<typename GService>
+    client_endpoint<GServiceStub>::client_endpoint(std::unique_ptr<GService> gservice,
+                                                   std::shared_ptr<cl::thread_pool> thread_pool,
+                                                   std::shared_ptr<iauthenticator> authenticator,
+                                                   std::shared_ptr<ipipe_env> pipe_env,
+                                                   state_change_callback_t &&handler,
+                                                   const std::chrono::milliseconds &req_timeout)
+        : m_service(std::make_shared<service>(std::move(gservice)))
+        , m_channel(std::make_unique<channel>())
+        , m_connector(std::make_shared<client_connector>(thread_pool, authenticator, pipe_env))
+        , m_thread_pool(thread_pool)
+        , m_change_callback(std::make_shared<state_change_callback_t>)
+        , m_request_timeout(req_timeout)
+        , m_service_stub(m_channel.get())
+    {}
+
+    template<typename GServiceStub>
+    void client_endpoint<GServiceStub>::connect()
+    {
+        auto connection0 = connection::create(m_thread_pool,
+                                              m_change_callback,
+                                              m_connector,
+                                              m_service,
+                                              m_request_timeout);
+        connection0->activate();
+        m_channel->set_connection(connection0);
+    }
+
+    template<typename GServiceStub>
+    void client_endpoint<GServiceStub>::disconnect()
+    {
+        m_channel->get_connection()->deactivate(); //TODO продумать
+    }
+
+    template<typename GServiceStub>
+    bool client_endpoint<GServiceStub>::is_connected() const
+    {
+        return m_channel->get_connection()->is_active();
+    }
+
+    template<typename GServiceStub>
     template<typename Request, typename Response>
-    std::unique_ptr<Response> client_endpoint::make_request(const Request &req,
-                                                            auto &service_stub,
-                                                            auto method)
+    std::unique_ptr<Response> client_endpoint<GServiceStub>::make_request(const Request &req,
+                                                                          auto method)
     {
         struct sync_req_data
         {
-            req_result_data<Response> data;
+            response_data<Response> data;
             cl::event sync_event;
         };
         auto sync_data = std::make_shared<sync_req_data>();
 
         auto req_callback =
             [sync_data] (request_result rc, std::unique_ptr<Response> response) {
-                sync_data->data.req_result = rc;
+                sync_data->data.result = rc;
                 sync_data->data.response = std::move(response);
                 sync_data->sync_event.set();
             };
 
         auto callback = request_callback<Response>::create_on_heap(std::move(req_callback));
 
-        (service_stub.*method)(callback,
-                               &req,
-                               callback->get_response_ptr(),
-                               callback);
+        (m_service_stub.*method)(callback,
+                                 &req,
+                                 callback->get_response_ptr(),
+                                 callback);
 
-        if(!sync_data->sync_event.wait_for(RequestTimeout * 2)) {
+        if(!sync_data->sync_event.wait_for(m_request_timeout * 2)) {
             throw request_exception(request_result::unknown_error,
                                     "waiting request callback failed");
         }
 
-        if(is_fail(sync_data->data.req_result)) {
-            throw request_exception(sync_data->data.req_result, "request failed");
+        if(is_fail(sync_data->data.result)) {
+            throw request_exception(sync_data->data.result, "request failed");
         }
 
         return std::move(sync_data->data.response);
     }
 
+    template<typename GServiceStub>
     template<typename Request, typename Response>
-    void client_endpoint::make_request_async(const Request &req,
-                                             auto &service_stub,
-                                             auto method,
-                                             request_callback_t<Response> &&req_callback)
+    void client_endpoint<GServiceStub>::make_request_async
+       (const Request &req,
+        auto method,
+        request_callback_t<Response> &&req_callback)
     {
         auto callback = request_callback<Response>::create_on_heap(std::move(req_callback));
 
-        (service_stub.*method)(callback,
-                               &req,
-                               callback->get_response_ptr(),
-                               callback);
+        (m_service_stub.*method)(callback,
+                                 &req,
+                                 callback->get_response_ptr(),
+                                 callback);
     }
 }
