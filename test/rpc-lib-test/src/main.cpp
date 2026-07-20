4fdfa6cde41aac3a62883c720a6f83a22b8b27b8
Viacheslav Shalygin <96007284+vshalygin@users.noreply.github.com>
2026-07-20 11:05:11 +0300

    Add mem leak check to rpc-lib-test

--- test/rpc-lib-test/src/main.cpp
+++ test/rpc-lib-test/src/main.cpp
@@ -1,6 +1,15 @@
 #include <gtest/gtest.h>
 
+#define _CRTDBG_MAP_ALLOC
+
+#include <crtdbg.h>
+#include <stdlib.h>
+
 GTEST_API_ int main(int argc, char **argv) {
+    _CrtSetDbgFlag(
+        _CRTDBG_ALLOC_MEM_DF |
+        _CRTDBG_LEAK_CHECK_DF);
+
     printf("Running main() from %s\n", __FILE__);
     testing::InitGoogleTest(&argc, argv);
 
