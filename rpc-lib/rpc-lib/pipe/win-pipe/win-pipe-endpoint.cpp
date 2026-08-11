6e36f8ce959c0b9e50913b34e4015bbb8616fe95
Viacheslav Shalygin <96007284+vshalygin@users.noreply.github.com>
2026-08-11 15:00:34 +0300

    Simplify win-pipe-endpoint

--- rpc-lib/rpc-lib/pipe/win-pipe/win-pipe-endpoint.cpp
+++ rpc-lib/rpc-lib/pipe/win-pipe/win-pipe-endpoint.cpp
@@ -5,8 +5,6 @@
 #include <condition_variable>
 
 namespace vshalygin::rpc {
-    using write_op = internal::win_pipe_write_operation;
-    using read_op = internal::win_pipe_read_operation;
     using op_res = internal::win_pipe_operation_res;
 
     namespace {
@@ -28,53 +26,17 @@ namespace vshalygin::rpc {
         }
     }
 
-    class win_pipe_endpoint::impl
-        : public std::enable_shared_from_this<impl>
+    std::shared_ptr<ipipe_endpoint> win_pipe_endpoint::create(win::pipe_handle &&handle,
+                                                              std::shared_ptr<internal::win_pipe_iocp_owner> iocp_owner,
+                                                              std::shared_ptr<cl::thread_pool> thread_pool)
     {
-    public:
-        explicit impl(win::pipe_handle &&pipe,
-                      std::shared_ptr<internal::win_pipe_iocp_owner> iocp_owner,
-                      std::shared_ptr<cl::thread_pool> thread_pool);
+        return std::shared_ptr<ipipe_endpoint>(
+            new win_pipe_endpoint(std::move(handle), std::move(iocp_owner), std::move(thread_pool)));
+    }
 
-        impl(const impl &) = delete;
-        impl &operator=(const impl &) = delete;
-
-        bool is_connected() const;
-        void set_disconnect_callback(cl::thread_pool_task<void()> &&callback);
-
-        write_future write_async(cl::buffer &&msg);
-        read_future read_async();
-        write_future write_async(cl::buffer &&msg, std::chrono::milliseconds timeout);
-        read_future read_async(std::chrono::milliseconds timeout);
-
-        void invalidate();
-
-    private:
-        void complete_write_op();
-        void complete_read_op();
-
-        void invoke_disconnect_callbacks();
-
-        void invalidate_impl(bool by_cancel);
-
-    private:
-        mutable std::mutex m_pipe_mtx;
-        std::vector<cl::thread_pool_task<void()>> m_disconnect_callbacks;
-        win::pipe_handle m_pipe;
-
-        std::shared_ptr<internal::win_pipe_iocp_owner> m_iocp_owner;
-        std::shared_ptr<cl::thread_pool> m_thread_pool;
-
-        mutable std::mutex m_write_op_mtx;
-        std::list<write_op> m_write_ops;
-
-        mutable std::mutex m_read_op_mtx;
-        std::list<read_op> m_read_ops;
-    };
-
-    win_pipe_endpoint::impl::impl(win::pipe_handle &&pipe,
-                                  std::shared_ptr<internal::win_pipe_iocp_owner> iocp_owner,
-                                  std::shared_ptr<cl::thread_pool> thread_pool)
+    win_pipe_endpoint::win_pipe_endpoint(win::pipe_handle &&pipe,
+                                         std::shared_ptr<internal::win_pipe_iocp_owner> iocp_owner,
+                                         std::shared_ptr<cl::thread_pool> thread_pool)
         : m_pipe(std::move(pipe))
         , m_iocp_owner(std::move(iocp_owner))
         , m_thread_pool(std::move(thread_pool))
@@ -82,13 +44,13 @@ namespace vshalygin::rpc {
         assert(m_pipe);
     }
 
-    bool win_pipe_endpoint::impl::is_connected() const
+    bool win_pipe_endpoint::is_connected() const
     {
         std::lock_guard g(m_pipe_mtx);
         return !m_pipe.empty();
     }
 
-    void win_pipe_endpoint::impl::set_disconnect_callback(cl::thread_pool_task<void()> &&callback)
+    void win_pipe_endpoint::set_disconnect_callback(cl::thread_pool_task<void()> &&callback)
     {
         std::lock_guard g(m_pipe_mtx);
         if(m_pipe) {
@@ -98,7 +60,7 @@ namespace vshalygin::rpc {
         }
     }
 
-    win_pipe_endpoint::write_future win_pipe_endpoint::impl::write_async(cl::buffer &&msg)
+    win_pipe_endpoint::write_future win_pipe_endpoint::write_async(cl::buffer &&msg)
     {
         std::lock_guard g(m_pipe_mtx);
         if(!m_pipe) {
@@ -123,7 +85,7 @@ namespace vshalygin::rpc {
                   });
     }
 
-    win_pipe_endpoint::read_future win_pipe_endpoint::impl::read_async()
+    win_pipe_endpoint::read_future win_pipe_endpoint::read_async()
     {
         std::lock_guard g(m_pipe_mtx);
         if(!m_pipe) {
@@ -148,26 +110,26 @@ namespace vshalygin::rpc {
                   });
     }
 
-    win_pipe_endpoint::write_future win_pipe_endpoint::impl::write_async(
+    win_pipe_endpoint::write_future win_pipe_endpoint::write_async(
         cl::buffer && /*msg*/, std::chrono::milliseconds /*timeout*/)
     {
         //TODO add definition
         return {};
     }
 
-    win_pipe_endpoint::read_future win_pipe_endpoint::impl::read_async(
+    win_pipe_endpoint::read_future win_pipe_endpoint::read_async(
         std::chrono::milliseconds /*timeout*/)
     {
         //TODO add definition
         return {};
     }
 
-    void win_pipe_endpoint::impl::invalidate()
+    void win_pipe_endpoint::invalidate()
     {
         invalidate_impl(true);
     }
 
-    void win_pipe_endpoint::impl::complete_write_op()
+    void win_pipe_endpoint::complete_write_op()
     {
         std::unique_lock g(m_write_op_mtx);
         m_write_ops.pop_front();
@@ -176,7 +138,7 @@ namespace vshalygin::rpc {
         }
     }
 
-    void win_pipe_endpoint::impl::complete_read_op()
+    void win_pipe_endpoint::complete_read_op()
     {
         std::unique_lock g(m_read_op_mtx);
         m_read_ops.pop_front();
@@ -185,7 +147,7 @@ namespace vshalygin::rpc {
         }
     }
 
-    void win_pipe_endpoint::impl::invoke_disconnect_callbacks()
+    void win_pipe_endpoint::invoke_disconnect_callbacks()
     {
         for(auto &cb : m_disconnect_callbacks) {
             cb.exec();
@@ -193,7 +155,7 @@ namespace vshalygin::rpc {
         m_disconnect_callbacks.clear();
     }
 
-    void win_pipe_endpoint::impl::invalidate_impl(bool by_cancel)
+    void win_pipe_endpoint::invalidate_impl(bool by_cancel)
     {
         std::lock_guard g(m_pipe_mtx);
         if(!m_pipe) {
@@ -218,54 +180,6 @@ namespace vshalygin::rpc {
         m_pipe.reset();
 
         invoke_disconnect_callbacks();
-    }
-
-    win_pipe_endpoint::win_pipe_endpoint(win::pipe_handle &&handle,
-                                         std::shared_ptr<internal::win_pipe_iocp_owner> iocp_owner,
-                                         std::shared_ptr<cl::thread_pool> thread_pool)
-        : m_impl(std::make_shared<impl>(std::move(handle), std::move(iocp_owner), std::move(thread_pool)))
-    {}
-
-    win_pipe_endpoint::~win_pipe_endpoint()
-    {
-        m_impl->invalidate();
-    }
-
-    bool win_pipe_endpoint::is_connected() const
-    {
-        return m_impl->is_connected();
-    }
-
-    void win_pipe_endpoint::set_disconnect_callback(cl::thread_pool_task<void()> &&callback)
-    {
-        m_impl->set_disconnect_callback(std::move(callback));
-    }
-
-    win_pipe_endpoint::write_future win_pipe_endpoint::write_async(cl::buffer &&msg)
-    {
-        return m_impl->write_async(std::move(msg));
-    }
-
-    win_pipe_endpoint::read_future win_pipe_endpoint::read_async()
-    {
-        return m_impl->read_async();
-    }
-
-    win_pipe_endpoint::write_future win_pipe_endpoint::write_async(
-        cl::buffer &&msg, std::chrono::milliseconds timeout)
-    {
-        return m_impl->write_async(std::move(msg), timeout);
-    }
-
-    win_pipe_endpoint::read_future win_pipe_endpoint::read_async(
-        std::chrono::milliseconds timeout)
-    {
-        return m_impl->read_async(timeout);
-    }
-
-    void win_pipe_endpoint::invalidate()
-    {
-        return m_impl->invalidate();
     }
 }
 #endif
--- rpc-lib/rpc-lib/pipe/win-pipe/win-pipe-endpoint.h
+++ rpc-lib/rpc-lib/pipe/win-pipe/win-pipe-endpoint.h
@@ -12,11 +12,15 @@
 namespace vshalygin::rpc {
     class win_pipe_endpoint
         : public ipipe_endpoint
+        , public std::enable_shared_from_this<win_pipe_endpoint>
     {
-    public:
         explicit win_pipe_endpoint(win::pipe_handle &&handle,
                                    std::shared_ptr<internal::win_pipe_iocp_owner> iocp_owner,
                                    std::shared_ptr<cl::thread_pool> thread_pool);
+    public:
+        static std::shared_ptr<ipipe_endpoint> create(win::pipe_handle &&handle,
+                                                      std::shared_ptr<internal::win_pipe_iocp_owner> iocp_owner,
+                                                      std::shared_ptr<cl::thread_pool> thread_pool);
 
         win_pipe_endpoint(const win_pipe_endpoint &) = delete;
         win_pipe_endpoint &operator=(const win_pipe_endpoint &) = delete;
@@ -34,8 +38,29 @@ namespace vshalygin::rpc {
         void invalidate() override;
 
     private:
-        class impl;
-        std::shared_ptr<impl> m_impl;
+        void complete_write_op();
+        void complete_read_op();
+
+        void invoke_disconnect_callbacks();
+
+        void invalidate_impl(bool by_cancel);
+
+    private:
+        using write_op = internal::win_pipe_write_operation;
+        using read_op = internal::win_pipe_read_operation;
+
+        mutable std::mutex m_pipe_mtx;
+        std::vector<cl::thread_pool_task<void()>> m_disconnect_callbacks;
+        win::pipe_handle m_pipe;
+
+        std::shared_ptr<internal::win_pipe_iocp_owner> m_iocp_owner;
+        std::shared_ptr<cl::thread_pool> m_thread_pool;
+
+        mutable std::mutex m_write_op_mtx;
+        std::list<write_op> m_write_ops;
+
+        mutable std::mutex m_read_op_mtx;
+        std::list<read_op> m_read_ops;
     };
 }
 
